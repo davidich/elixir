915be4b7d00fe8a7e69da58111768135f4bac544
Oleksiy Davidich <oleksiy.davidich@gmail.com>
2013-01-27 21:36:41 +0200

    elexir.html is cleaned up

--- APPLICATION/Scripts/main.js
+++ APPLICATION/Scripts/main.js
@@ -1,6 +1,14 @@
-﻿// if console.log does't exists(IE)
-if (!console || !console.log || typeof console.log != "function")
-    console = { log: function () { } };
+﻿// if console doesn't exists(IE)
+if (!window.console) {
+    window.console = {};
+}
+var debugLevels = ["log", "info", "warn", "error", "assert", "dir", "clear", "profile", "profileEnd"];
+for (var i = 0; i < debugLevels.length; i++) {
+    if (!window.console[debugLevels[i]] || typeof window.console[debugLevels[i]] != "function")
+        window.console[debugLevels[i]] = function() {
+        };
+}
+
 
 // place to keep some global values (I doubt we'll need it but let's have it for now)
 GLOBAL =
@@ -41,11 +49,77 @@ requirejs.config(config);
 
 
 // Start the main app logic.
-requirejs(['jquery'],
-function ($) {
-    $.ajaxSetup({
-        cache: false
+require(["jquery", "domReady", "sm2"],
+    function ($, domReady, sm2) {
+        
+        $.ajaxSetup({
+            cache: false
+        });
+
+        domReady(function () {
+            $("#playPause").click(function () {
+                var buttonText = $(this).text();
+                if (buttonText == ">")
+                    play();
+                else
+                    pause();
+            });
+
+            $("#stop").click(stop);
+
+            $("#muteUnmute").click(function () {
+                var button = $(this);
+
+                if (!sm2.muted)
+                    sm2.mute();
+                else
+                    sm2.unmute();
+
+                button.text(button.text() == "mute" ? "unmute" : "mute");
+                sm2.toggleMute("curSound");
+            });
+
+            $("#songs button").click(function () { playSong(this); });
+        });
+
+        function play() {
+            var soundUrl = $("#soundUrl").val();
+            if (!sm2.canPlayURL(soundUrl)) {
+                alert("bad url");
+                return;
+            }
+
+            $("#playPause").text("||");
+
+            var curSong = sm2.getSoundById("curSong");
+            if (curSong && curSong.url == soundUrl)
+                curSong.resume();
+            else {
+                sm2.destroySound("curSong");
+
+                var curSoung = sm2.createSound({
+                    id: 'curSong',
+                    url: soundUrl
+                });
+
+                curSoung.play();
+            }
+        }
+
+        function pause() {
+            $("#playPause").text(">");
+            sm2.pauseAll();
+        }
+
+        function playSong(songButton) {
+            var $button = $(songButton);
+            $("#soundUrl").val($button.data("url"));
+            play();
+        }
+
+        function stop() {
+            $("#soundUrl").val("");
+            $("#playPause").text(">");
+            sm2.destroySound("curSong");
+        }
     });
-
-
-});
