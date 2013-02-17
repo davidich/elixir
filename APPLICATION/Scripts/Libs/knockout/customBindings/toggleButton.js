8b427d4e3bcd787e7c64b9cef2956057051986f7
Oleksiy Davidich <oleksiy.davidich@gmail.com>
2013-02-17 18:30:13 +0200

    toggleButton custom ko binding is added

---
+++ APPLICATION/Scripts/Libs/knockout/customBindings/toggleButton.js
@@ -0,0 +1,34 @@
+﻿define(["knockout"], function (ko) {
+
+    ko.bindingHandlers['toggleButton'] = {
+        init: function (element, valueAccessor, allBindingsAccessor) {
+            var $element = $(element);
+            var value = valueAccessor();
+            
+            $(element)
+                .click(function() {
+                    value(!value());
+                })
+                .data("activeCssClass", allBindingsAccessor().activeCss || "active")
+                .data("inactiveCssClass", allBindingsAccessor().inactiveCss || "inactive");
+            
+            setState($element, value());
+        },
+        update: function (element, valueAccessor, allBindingsAccessor) {
+            var $element = $(element);
+            var value = valueAccessor();
+            var toggleCallback = allBindingsAccessor().onToggle;
+            setState($element, value());
+
+            if (typeof toggleCallback == "function")
+                toggleCallback();
+        }
+    };
+    
+    function setState($elem, state) {
+        if (state)
+            $elem.addClass($elem.data("activeCssClass")).removeClass($elem.data("inactiveCssClass"));
+        else
+            $elem.addClass($elem.data("inactiveCssClass")).removeClass($elem.data("activeCssClass"));       
+    }
+})
--- APPLICATION/Scripts/Vms/playerVM.js
+++ APPLICATION/Scripts/Vms/playerVM.js
@@ -41,14 +41,49 @@ function (ko, viewBase, pubSub, Track, TrackForPlayer, sequenceManager) {
     function Player() {
         var self = this;
 
-        // Data
+
+        // override base isVisible logic
+        self.isVisible = ko.observable();
+        pubSub.sub("viewChanged", function (viewName) {
+            self.isVisible($.inArray(viewName, ["music", "video", "artists"]) != -1);
+        });
+        
+
+        // Build UI
+        var $slider = $('.trackSlider').slider({
+            range: "min",
+            min: 0,
+            max: 0,
+            value: 0,
+            animate: true,
+            slide: function (event, ui) { self.refreshPosition(ui.value); }
+        });
+        
+        var $volume = $('.trackVolume').slider({
+            range: "min",
+            min: 0,
+            max: 100,
+            value: 100,
+            animate: true,
+            slide: function (event, ui) {
+                self.isMuted(false);
+                self.volume(ui.value);
+                self.refreshVolume();
+            }
+        });
+        
+
+        // DATA
         self.track = ko.observable();
         self.tracks = ko.observableArray();
-        self.state = ko.observable("paused"); //playing, paused
+        self.state = ko.observable("paused"); //playing, paused, stoped
         self.isElapsed = ko.observable(true);
         self.isShuffled = ko.observable(false);
+        self.isLooped = ko.observable(false);
         self.position = ko.observable(0);
-        self.time = ko.computed(function() {
+        self.isMuted = ko.observable(false);        
+        self.volume = ko.observable(100);                
+        self.time = ko.computed(function () {
             if (!self.track()) return "0:00";
 
             var pos;
@@ -58,119 +93,92 @@ function (ko, viewBase, pubSub, Track, TrackForPlayer, sequenceManager) {
             } else {
                 pos = self.track().duration() - self.position();
                 return "-" + Track.toTimeString(pos);
-            }            
+            }
         });
-        self.isMuted = ko.observable(false);
-        
-        self.volume = ko.observable(100);
+        self.sound = function () {
+            if (!self.track())
+                return null;
 
-        // override base isVisible logic
-        self.isVisible = ko.observable();
-        pubSub.sub("viewChanged", function (viewName) {
-            self.isVisible($.inArray(viewName, ["music", "video", "artists"]) != -1);
-        });
+            var id = self.track().id();
+            var url = self.track().url();
+
+            return soundManager.getSoundById(id)
+                || soundManager.createSound({
+                    id: id,
+                    url: url,
+                    whileplaying: function () {
+                        var posInSecs = this.position / 1000;
+                        self.refreshPosition(posInSecs, true /*skipSoundUpdate*/);
+                    }
+                });
+        };
+        
 
         // BEHAVIOR        
         self.playPause = function () {
-            if (self.state() == "playing") pause();
-            else play();
+            if (self.state() != "playing")
+                play();
+            else
+                pause();
         };
 
         self.next = function () {
-            playNext();
+            if (!self.track()) return;
+
+            var curIndex = self.tracks.indexOf(self.track());
+            var trackLength = self.tracks().length;
+            var isShuffled = self.isShuffled();
+            var newIndex = sequenceManager.getNext(curIndex, trackLength, isShuffled);
+
+            self.track(self.tracks()[newIndex]);
+            if (self.state() == "playing") play();
         };
 
         self.prev = function () {
-            playPrev();
-        };
-
-        self.test = function () {
-            $slider.slider("option", "max", 150);
-        };
-
-        self.mute = function() {
-            self.isMuted(!self.isMuted());
-            refreshVolume();
-        };
-
-        self.switchTimeMode = function() {
-            self.isElapsed(!self.isElapsed());
-        };
-
-        self.shuffle = function() {
-            self.isShuffled(!self.isShuffled());
-        };
-        
-        // track slider
-        var $slider = $('.trackSlider').slider({
-            range: "min",
-            min: 0,
-            max: 0,
-            value: 0,
-            animate: true,
-            slide: function(event, ui) { refreshPosition(ui.value); }
-        });
-        
-        function setSliderDuration(duration) {
-            $slider.slider("option", "max", duration);
-        }
-
-        function refreshPosition(positionInSecs, skipSoundUpdate) {
             if (!self.track()) return;
-            
-            
-            var sound = soundManager.getSoundById(self.track().id());
-            
-            // prevent sliding over the loaded part
-            var maxPosition = sound.duration / 1000;
-            if (positionInSecs > maxPosition) positionInSecs = maxPosition;                       
-            
+
+            var curIndex = self.tracks.indexOf(self.track());
+            var trackLength = self.tracks().length;
+            var isShuffled = self.isShuffled();
+            var newIndex = sequenceManager.getPrev(curIndex, trackLength, isShuffled);
+
+            self.track(self.tracks()[newIndex]);
+            if (self.state() == "playing") play();
+        };
+
+        self.refreshVolume = function () {
+            var resolvedVolume = !self.isMuted()
+                ? self.volume()
+                : 0;
+
+            // UI
+            if ($volume.slider("option", "value") != resolvedVolume)
+                $volume.slider("option", "value", resolvedVolume);
+
             // functional update
-            if (!skipSoundUpdate)
-                sound.setPosition(positionInSecs * 1000);
+            var sound = self.sound();
+            if (sound && sound.volume != resolvedVolume) sound.setVolume(resolvedVolume);
+        };
+
+        self.refreshPosition = function(positionInSecs, skipSoundUpdate) {
+            var sound = self.sound();
+            if (!sound) return;
+
+            // prevent sliding over the loaded part            
+            var maxPosition = sound.duration / 1000;
+            if (positionInSecs > maxPosition) positionInSecs = maxPosition;
 
             // UI
             if ($slider.slider("option", "value") != positionInSecs)
                 $slider.slider("option", "value", positionInSecs);
 
-            self.position(parseInt(positionInSecs));
-        }
-        
-        // volume
-        var $volume = $('.trackVolume').slider({
-            range: "min",
-            min: 0,
-            max: 100,
-            value: 100,
-            animate: true,
-            slide: function(event, ui) { onVolumeChanged(ui.value); }
-        });
-        
-        function onVolumeChanged(volume) {
-            self.isMuted(false);
-            if (!self.track()) return;
-
-            self.volume(volume);
-            refreshVolume();
-        }
-
-        function refreshVolume() {
-            var sound = soundManager.getSoundById(self.track().id());
-
-            var resolvedVolume = !self.isMuted()
-                ? self.volume()
-                : 0;
-            
             // functional update
-            if (sound.volume != resolvedVolume)
-                sound.setVolume(resolvedVolume);
-
-            // UI
-            if ($volume.slider("option", "value") != resolvedVolume)
-                $volume.slider("option", "value", resolvedVolume);
-        }
+            if (!skipSoundUpdate) sound.setPosition(positionInSecs * 1000);
+            self.position(parseInt(positionInSecs));
+        };
         
-        // Events
+
+        // EVENTS
         pubSub.sub("track.onPlayClick", function (track) {
             var tr = new TrackForPlayer(track);
             self.tracks.push(tr);
@@ -181,7 +189,7 @@ function (ko, viewBase, pubSub, Track, TrackForPlayer, sequenceManager) {
         pubSub.sub("trackForPlayer.onDeleteClick", function (deletedTrack) {
             // try to start playing next track
             if (self.track() == deletedTrack && self.tracks().length > 1)
-                playNext();
+                self.next();
 
             // is that was last instance of that track in playlist?
             var itemsWithTheSameId = $.grep(self.tracks(), function (elem) { return elem.id() == deletedTrack.id(); });
@@ -192,76 +200,44 @@ function (ko, viewBase, pubSub, Track, TrackForPlayer, sequenceManager) {
             self.tracks.splice(index, 1);
         });
 
+
         // Helpers
         function play() {
-            if (!self.track()) return;            
-            self.state("playing");
-
-            var track = self.track(),
-                url = track.url(),
-                sound = soundManager.getSoundById(track.id());
-
-            if (!sound) {
-                sound = soundManager.createSound({
-                    id: track.id(),
-                    url: url,
-                    whileplaying: function () {
-                        var posInSecs = this.position / 1000;
-                        refreshPosition(posInSecs, true /*skipSoundUpdate*/);
-                    }
-                });                               
-            }
-
-            sound.setVolume(self.volume());
+            var sound = self.sound();
+            if (!sound) return;
 
             if (sound.paused) {
                 sound.resume();
             } else {
                 stop();
-                soundManager.play(track.id());
+                sound.play();
+                refreshSliderLength();
             }
-            
-            setSliderDuration(track.duration());        //do it as after stop is called
+
+            self.state("playing");
         }
 
         function stop() {
+            self.state("stoped");
             if (!self.track()) return;
             
             soundManager.stopAll();
-            setSliderDuration(0);
-        }
-
-        function playNext() {
-            if (!self.track()) return;
-
-            var curIndex = self.tracks.indexOf(self.track());
-            var trackLength = self.tracks().length;
-            var isShuffled = self.isShuffled();
-            var newIndex = sequenceManager.getNext(curIndex, trackLength, isShuffled);
-            
-            self.track(self.tracks()[newIndex]);
-            if (self.state() == "playing") play();
-        }
-
-        function playPrev() {
-            if (!self.track()) return;
-
-            var curIndex = self.tracks.indexOf(self.track());
-            var trackLength = self.tracks().length;
-            var isShuffled = self.isShuffled();
-            var newIndex = sequenceManager.getPrev(curIndex, trackLength, isShuffled);
-
-            self.track(self.tracks()[newIndex]);
-            if (self.state() == "playing") play();
+            refreshSliderLength();
         }
 
         function pause() {
-            if (!self.track()) return;
-
-            var track = self.track();
-            var sound = soundManager.getSoundById(track.id());
+            var sound = self.sound();
+            if (!sound) return;
+            
             sound.pause();
             self.state("paused");
+        }
+
+        function refreshSliderLength() {
+            if(!self.track() || self.state() == "stopped")
+                $slider.slider("option", "max", 0);
+            else
+                $slider.slider("option", "max", self.track().duration());
         }
        
     }
